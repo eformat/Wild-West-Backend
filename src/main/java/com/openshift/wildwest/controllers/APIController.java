f8c4e5620e16b6fb73e7be9bc2f20c9e36268e4a
Mike Hepburn <eformat@gmail.com>
2019-07-22 16:03:45 +1000

    sydney

--- src/main/java/com/openshift/wildwest/controllers/APIController.java
+++ src/main/java/com/openshift/wildwest/controllers/APIController.java
@@ -1,20 +1,19 @@
 package com.openshift.wildwest.controllers;
 
 import java.util.List;
-
+import com.openshift.wildwest.helpers.PlatformObjectHelper;
+import com.openshift.wildwest.models.Game;
+import com.openshift.wildwest.models.PlatformObject;
+import com.openshift.wildwest.models.Score;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import com.openshift.wildwest.helpers.PlatformObjectHelper;
-import com.openshift.wildwest.models.Game;
-import com.openshift.wildwest.models.Score;
-import com.openshift.wildwest.models.PlatformObject;
 
 
-	// oc policy add-role-to-user view system:serviceaccount:wildwest:default where wildwest
-	// is the project name
+	// oc policy add-role-to-user view system:serviceaccount:wildwest:default
+	// where wildwest is the project name
 
 	// To enable destructrive mode, a different permission needs to be added 
 	// oc policy add-role-to-user edit system:serviceaccount:wildwest:default
--- src/main/java/com/openshift/wildwest/helpers/PlatformObjectHelper.java
+++ src/main/java/com/openshift/wildwest/helpers/PlatformObjectHelper.java
@@ -16,119 +16,119 @@ import io.fabric8.openshift.client.OpenShiftClient;
 
 public class PlatformObjectHelper {
 
-	private OpenShiftClient client;
+  private OpenShiftClient client;
 
-	public PlatformObjectHelper() {
-		client = new DefaultOpenShiftClient();
-	}
+  public PlatformObjectHelper() {
+    client = new DefaultOpenShiftClient();
+  }
 
-	public List<PlatformObject> getPlatformObjects() {
+  public List<PlatformObject> getPlatformObjects() {
 
-		ArrayList<PlatformObject> platformObjects = new ArrayList<>();
-		platformObjects.addAll(this.getPods());
-		platformObjects.addAll(this.getBuilds());
-		platformObjects.addAll(this.getDeploymentConfigs());
-		platformObjects.addAll(this.getBuildConfigs());
-		platformObjects.addAll(this.getPVs());
-		platformObjects.addAll(this.getServices());
-		platformObjects.addAll(this.getRoutes());
+    ArrayList<PlatformObject> platformObjects = new ArrayList<>();
+    platformObjects.addAll(this.getPods());
+    platformObjects.addAll(this.getBuilds());
+    platformObjects.addAll(this.getDeploymentConfigs());
+    platformObjects.addAll(this.getBuildConfigs());
+    platformObjects.addAll(this.getPVs());
+    platformObjects.addAll(this.getServices());
+    platformObjects.addAll(this.getRoutes());
 
-		return platformObjects;
+    return platformObjects;
+  }
 
-	}
+  public PlatformObject getRandomPlatformObject() {
+    List<PlatformObject> theObjects = this.getPlatformObjects();
 
-	public PlatformObject getRandomPlatformObject() {
-		List<PlatformObject> theObjects = this.getPlatformObjects();
-		
+    return theObjects.get(new Random().nextInt(theObjects.size()));
+  }
 
-		return theObjects.get(new Random().nextInt(theObjects.size()));
-	}
+  private List<PlatformObject> getPods() {
+    ArrayList<PlatformObject> thePods = new ArrayList<>();
 
-	private List<PlatformObject> getPods() {
-		ArrayList<PlatformObject> thePods = new ArrayList<>();
+    List<Pod> pods = client.pods().list().getItems();
+    for (Pod currPod : pods) {
+      thePods.add(new PlatformObject(currPod.getMetadata().getUid(),
+          currPod.getMetadata().getName(), "POD"));
+    }
+    return thePods;
+  }
 
-		List<Pod> pods = client.pods().list().getItems();
-		for (Pod currPod : pods) {
-			thePods.add(new PlatformObject(currPod.getMetadata().getUid(), currPod.getMetadata().getName(), "POD"));
-		}
-		return thePods;
-	}
+  private List<PlatformObject> getBuilds() {
+    ArrayList<PlatformObject> theBuilds = new ArrayList<>();
 
-	private List<PlatformObject> getBuilds() {
-		ArrayList<PlatformObject> theBuilds = new ArrayList<>();
+    List<Build> builds = client.builds().list().getItems();
+    for (Build currBuild : builds) {
+      theBuilds.add(new PlatformObject(currBuild.getMetadata().getUid(),
+          currBuild.getMetadata().getName(), "BUILD"));
+    }
 
-		List<Build> builds = client.builds().list().getItems();
-		for (Build currBuild : builds) {
-			theBuilds.add(
-					new PlatformObject(currBuild.getMetadata().getUid(), currBuild.getMetadata().getName(), "BUILD"));
-		}
+    return theBuilds;
+  }
 
-		return theBuilds;
-	}
+  private List<PlatformObject> getDeploymentConfigs() {
+    ArrayList<PlatformObject> theDeployments = new ArrayList<>();
+    List<DeploymentConfig> deploymentConfigs = client.deploymentConfigs().list().getItems();
+    for (DeploymentConfig currConfig : deploymentConfigs) {
+      theDeployments.add(new PlatformObject(currConfig.getMetadata().getUid(),
+          currConfig.getMetadata().getName(), "DEPLOYMENT_CONFIG"));
+    }
 
-	private List<PlatformObject> getDeploymentConfigs() {
-		ArrayList<PlatformObject> theDeployments = new ArrayList<>();
-		List<DeploymentConfig> deploymentConfigs = client.deploymentConfigs().list().getItems();
-		for (DeploymentConfig currConfig : deploymentConfigs) {
-			theDeployments.add(new PlatformObject(currConfig.getMetadata().getUid(), currConfig.getMetadata().getName(),
-					"DEPLOYMENT_CONFIG"));
-		}
+    return theDeployments;
+  }
 
-		return theDeployments;
-	}
+  private List<PlatformObject> getBuildConfigs() {
+    ArrayList<PlatformObject> theList = new ArrayList<>();
+    List<BuildConfig> theItems = client.buildConfigs().list().getItems();
+    for (BuildConfig currConfig : theItems) {
+      theList.add(new PlatformObject(currConfig.getMetadata().getUid(),
+          currConfig.getMetadata().getName(), "BUILD_CONFIG"));
+    }
+    return theList;
+  }
 
-	private List<PlatformObject> getBuildConfigs() {
-		ArrayList<PlatformObject> theList = new ArrayList<>();
-		List<BuildConfig> theItems = client.buildConfigs().list().getItems();
-		for (BuildConfig currConfig : theItems) {
-			theList.add(new PlatformObject(currConfig.getMetadata().getUid(), currConfig.getMetadata().getName(),
-					"BUILD_CONFIG"));
-		}
-		return theList;
-	}
+  private List<PlatformObject> getPVs() {
+    ArrayList<PlatformObject> theList = new ArrayList<>();
+    List<PersistentVolumeClaim> theItems = client.persistentVolumeClaims().list().getItems();
+    for (PersistentVolumeClaim currConfig : theItems) {
+      theList.add(new PlatformObject(currConfig.getMetadata().getUid(),
+          currConfig.getMetadata().getName(), "PVC"));
+    }
+    return theList;
+  }
 
-	private List<PlatformObject> getPVs() {
-		ArrayList<PlatformObject> theList = new ArrayList<>();
-		List<PersistentVolumeClaim> theItems = client.persistentVolumeClaims().list().getItems();
-		for (PersistentVolumeClaim currConfig : theItems) {
-			theList.add(
-					new PlatformObject(currConfig.getMetadata().getUid(), currConfig.getMetadata().getName(), "PVC"));
-		}
-		return theList;
-	}
+  private List<PlatformObject> getServices() {
+    ArrayList<PlatformObject> theList = new ArrayList<>();
+    List<Service> theItems = client.services().list().getItems();
+    for (Service currConfig : theItems) {
+      theList.add(new PlatformObject(currConfig.getMetadata().getUid(),
+          currConfig.getMetadata().getName(), "SERVICE"));
+    }
+    return theList;
+  }
 
-	private List<PlatformObject> getServices() {
-		ArrayList<PlatformObject> theList = new ArrayList<>();
-		List<Service> theItems = client.services().list().getItems();
-		for (Service currConfig : theItems) {
-			theList.add(new PlatformObject(currConfig.getMetadata().getUid(), currConfig.getMetadata().getName(),
-					"SERVICE"));
-		}
-		return theList;
-	}
+  private List<PlatformObject> getRoutes() {
+    ArrayList<PlatformObject> theList = new ArrayList<>();
+    List<Route> theItems = client.routes().list().getItems();
+    for (Route currConfig : theItems) {
+      theList.add(new PlatformObject(currConfig.getMetadata().getUid(),
+          currConfig.getMetadata().getName(), "ROUTE"));
+    }
+    return theList;
+  }
 
-	private List<PlatformObject> getRoutes() {
-		ArrayList<PlatformObject> theList = new ArrayList<>();
-		List<Route> theItems = client.routes().list().getItems();
-		for (Route currConfig : theItems) {
-			theList.add(
-					new PlatformObject(currConfig.getMetadata().getUid(), currConfig.getMetadata().getName(), "ROUTE"));
-		}
-		return theList;
-	}
+  public void deletePlatformObject(String gameID, String objectID, String objectType,
+      String objectName) {
 
-	public void deletePlatformObject(String gameID, String objectID, String objectType, String objectName) {
+    // right now only delete things we can recover from
+    switch (objectType) {
+      case "POD":
+        client.pods().withName(objectName).delete();
+        break;
+      case "BUILD":
+        client.builds().withName(objectName).delete();
+        break;
+    }
 
-		// right now only delete things we can recover from
-		switch (objectType) {
-			case "POD":
-				client.pods().withName(objectName).delete();
-				break;
-			case "BUILD":
-				client.builds().withName(objectName).delete();
-				break;
-		}
-		
-	}
+  }
 
 }
